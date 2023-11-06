a83c09b751efd97deeb659c73250e1f04c96134d
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-11-06 15:24:20 +0530

    Red : Get positions of all X's
    Implement function test_get_element_positions

--- test_life.py
+++ test_life.py
@@ -19,3 +19,15 @@ O   O   O   O   O   O   O
 O   O   O   O   O   O   O   
     Press space for next
     Press q for quit"""
+
+def test_get_element_positions():
+    matrix = [
+        ["O","O","O","O","O","O","O"],
+        ["O","O","O","O","O","O","O"],
+        ["O","O","O","O","O","O","O"],
+        ["O","O","X","X","X","O","O"],
+        ["O","O","O","O","O","O","O"],
+        ["O","O","O","O","O","O","O"],
+        ["O","O","O","O","O","O","O"]
+    ]
+    assert get_element_positions(matrix) == [[3, 2], [3, 3], [3, 4]]
