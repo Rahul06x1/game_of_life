2e16201d5cbfc9779b319c3d59270721200aa2b8
Rahul Rajeev <rahulrajeev2k1@gmail.com>
2023-11-06 19:15:25 +0530

    Red : Kill alive cells having more than three alive neighbor cells
    Implement function test_update_matrix_more_than_three_alive_neighbors

--- test_life.py
+++ test_life.py
@@ -42,16 +42,11 @@ def test_update_matrix_less_than_two_alive_neighbors():
         ["O","O","O","X","O","O","O"],
         ["O","O","O","O","O","O","O"],
         ]
-    expected_matrix = [
-        ['O', 'O', 'O', 'O', 'O', 'O', 'O'],
-        ['O', 'O', 'O', 'O', 'O', 'O', 'O'], 
-        ['O', 'O', 'O', 'X', 'O', 'O', 'O'], 
-        ['O', 'O', 'X', 'X', 'X', 'O', 'O'], 
-        ['O', 'O', 'O', 'X', 'O', 'O', 'O'], 
-        ['O', 'O', 'O', 'O', 'O', 'O', 'O'], 
-        ['O', 'O', 'O', 'O', 'O', 'O', 'O']
-        ]
-    assert update_matrix(matrix) == expected_matrix
+    matrix = update_matrix(matrix)
+    assert matrix[1][3] == DEAD
+    assert matrix[3][1] == DEAD
+    assert matrix[3][5] == DEAD
+    assert matrix[5][3] == DEAD
 
 def test_update_matrix_alive_cell_in_sides():
     matrix = [
@@ -69,6 +64,28 @@ def test_update_matrix_alive_cell_in_sides():
         ["O","O","O","O","O","O","O"],
         ["O","O","O","O","O","O","O"],
         ["O","O","O","O","O","O","O"],
+        ["O","O","O","O","O","O","O"],
+        ["O","O","O","O","O","O","O"]
+    ]
+
+    assert update_matrix(matrix) == expected_matrix
+
+def test_update_matrix_more_than_three_alive_neighbors():
+    matrix = [
+        ["O","O","O","O","O","O","O"],
+        ["O","O","O","O","O","O","O"],
+        ["O","O","O","O","X","O","O"],
+        ["O","O","X","X","X","O","O"],
+        ["O","O","X","O","O","O","O"],
+        ["O","O","O","O","O","O","O"],
+        ["O","O","O","O","O","O","O"]
+    ]
+    expected_matrix = [
+        ["O","O","O","O","O","O","O"],
+        ["O","O","O","O","O","O","O"],
+        ["O","O","O","O","X","O","O"],
+        ["O","O","X","O","X","O","O"],
+        ["O","O","X","O","O","O","O"],
         ["O","O","O","O","O","O","O"],
         ["O","O","O","O","O","O","O"]
     ]
